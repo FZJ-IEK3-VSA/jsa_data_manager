cae3b54c345483207190c82ba1f01f3e6d7d9277
julian-belina <56728940+julian-belina@users.noreply.github.com>
2024-06-20 18:55:44 +0200

    inter

--- src/jsa_data_manager/data_types.py
+++ src/jsa_data_manager/data_types.py
@@ -1,6 +1,9 @@
-import datetime
 import dataclasses
+import datetime
 from dataclasses import dataclass
+from enum import StrEnum
+from typing import Literal
+
 import pandas
 
 
@@ -27,36 +30,79 @@ class TimeSeriesColumnEntryMetaData:
     sum: float | None = None
 
 
+# @dataclass(kw_only=True)
+# class TimeSeriesColumnEntryMetaData:
+#     column_number: int
+#     column_name: str
+#     unit: str
+#     # optional
+#     description: str | None = "Electricity"
+#     tags: list[str] | None = None
+#     min: float | None = None
+#     max: float | None = None
+#     sum: float | None = None
+
+
+class DataSourceTypes(StrEnum):
+    SOFTWARE_SOURCE = "DataSource"
+    DATA_SOURCE = "SoftwareSource"
+
+
 @dataclass(kw_only=True)
-class SoftwareRunData:
-    data_source: str
-    software_version: str
-    user_name: str
-    run_guid: str
+class DataSource:
+    source_type: Literal[DataSourceTypes.DATA_SOURCE]
+    source_name: str
+    reference: str  # URL, doi or other reference to the data set
+    guid: str
 
 
-# software_run_data = SoftwareRunData(
-#     data_source="FINE",
-#     software_version="2.23.3",
-#     user_name="k-schulze",
-#     run_guid="1f2e2503-d0db-4736-97f2-c1e504b8dbcb",
-# )
+@dataclass(kw_only=True)
+class SoftwareSource:
+    source_type: Literal[DataSourceTypes.SOFTWARE_SOURCE]
+    software_name: str
+    version: str  # 2.2.1
+    user_name: (
+        str  # Your Linux User Name on the cluster that was used to create the data
+    )
+    guid: str
+
+
+# @dataclass(kw_only=True)
+# class DataSource:
+#     source_name: SMARD
+#     description: https://www.smard.de/page/home/marktdaten/78?marketDataAttributes=%7B%22resolution%22:%22hour%22,%22from%22:1717970400000,%22to%22:1718920799999,%22moduleIds%22:%5B1004066,1001226,1001225,1004067,1004068,1001228,1001223,1004069,1004071,1004070,1001227,5000410,6000411,2005097%5D,%22selectedCategory%22:1,%22activeChart%22:false,%22style%22:%22color%22,%22categoriesModuleOrder%22:%7B%7D,%22region%22:%22DE%22%7D
+#     user_name: k-schulze
+#     guid: 530cd955-a392-4c48-8355-f7753ac3a3e3
+
+# @dataclass(kw_only=True)
+# class SoftwareSource:
+#     software_name="FINE"
+#     version: "2.2.1"
+#     user_name: j-belina
+#     run_guid: "1f2e2503-d0db-4736-97f2-c1e504b8dbcb"
+
+
+class TimeSeriesStandards(StrEnum):
+    V1_0 = "JSA TimeSeriesStandard 1.0"
 
 
 @dataclass(kw_only=True)
 class TimeSeriesFileMetaData:
     name: str
     data_frame: pandas.DataFrame
-    software_run_data: SoftwareRunData
-    first_start_time: datetime.datetime
-    last_end_time: datetime.datetime
+    data_source: DataSource | SoftwareSource
     time_stamp_column_meta_data: TimeStampColumnMetaData
     column_list: list[TimeSeriesColumnEntryMetaData]
-    data_format_standard: str
+    delimiter: Literal[","]
+    data_format_standard: Literal[TimeSeriesStandards.V1_0]
 
 
-# import subprocess
-
-# res = subprocess.run(["git", "config", "user.name"], stdout=subprocess.PIPE)
-# git_username = res.stdout.strip().decode()
-# print(git_username)
+# @dataclass(kw_only=True)
+# class TimeSeriesFileMetaData:
+#     name = "Private Household Profile 2022"
+#     data_frame: pandas.DataFrame
+#     data_source: SoftwareSource
+#     time_stamp_column_meta_data: TimeStampColumnMetaData
+#     column_list: list[TimeSeriesColumnEntryMetaData]
+#     delimiter = ","
+#     data_format_standard= "JSA TimeSeriesStandard 1.0"
--- src/jsa_data_manager/jsa_data_manager.py
+++ src/jsa_data_manager/jsa_data_manager.py
@@ -1,22 +1,17 @@
 import pandas
 import json
-
+import pathlib
+import csv
 from jsa_data_manager.data_types import (
     TimeSeriesColumnEntryMetaData,
-    SoftwareRunData,
+    SoftwareSource,
+    DataSource,
     TimeSeriesFileMetaData,
     TimeStampColumnMetaData,
+    DataSourceTypes,
+    TimeSeriesStandards,
 )
-
-
-# class DataManagerBase:
-#     def read_csv_to_data_frame(self, path: str) -> pandas.DataFrame:
-#         data_frame = pandas.read_csv(io=path)
-#         return data_frame
-
-#     def read_excel_to_data_frame(self, path: str) -> pandas.DataFrame:
-#         data_frame = pandas.read_excel(io=path)
-#         return data_frame
+from jsa_data_manager.warning_and_error import CorruptedData, IncompleteData
 
 
 class LoadProfileDataManager:
@@ -29,9 +24,41 @@ class LoadProfileDataManager:
             meta_data_dictionary = json.load(json_file)
         return meta_data_dictionary
 
-    def read_load_profile_data(
-        self, path_to_data_frame: str, path_to_meta_data: str
-    ) -> TimeSeriesFileMetaData:
+    def _convert_meta_data_path_csv(self, path_to_meta_data: str | pathlib.Path) -> str:
+        if isinstance(path_to_meta_data, str):
+            path_to_meta_data = pathlib.Path(path_to_meta_data)
+        path_to_meta_data_without_file_extension = path_to_meta_data.stem
+        path_to_csv_data_str = str(path_to_meta_data_without_file_extension) + ".csv"
+        path_to_csv_data = pathlib.Path(path_to_csv_data_str)
+        if not path_to_csv_data.is_file():
+            raise Exception("No csv file exists at: " + path_to_csv_data_str)
+        return path_to_csv_data_str
+
+    def read_to_data_frame(
+        self, path_to_meta_data: str | pathlib.Path
+    ) -> pandas.DataFrame:
+        meta_data_dictionary = self.read_meta_data_dictionary(
+            path_to_meta_data=path_to_meta_data
+        )
+        time_stamp_column_meta_data = self.get_time_stamp_column_meta_data(
+            meta_data_dictionary=meta_data_dictionary
+        )
+        path_to_csv_file = self._convert_meta_data_path_csv(
+            path_to_meta_data=path_to_meta_data
+        )
+        date_columns = [
+            time_stamp_column_meta_data.start_column_name,
+            time_stamp_column_meta_data.end_column_name,
+        ]
+        data_frame = pandas.read_csv(
+            filepath_or_buffer=path_to_csv_file,
+            delimiter=",",
+            parse_dates=date_columns,
+            index_col=time_stamp_column_meta_data.index_column_name,
+        )
+        return data_frame
+
+    def read_to_dataclass(self, path_to_meta_data: str) -> TimeSeriesFileMetaData:
         meta_data_dictionary = self.read_meta_data_dictionary(
             path_to_meta_data=path_to_meta_data
         )
@@ -42,16 +69,18 @@ class LoadProfileDataManager:
             time_stamp_column_meta_data.start_column_name,
             time_stamp_column_meta_data.end_column_name,
         ]
+
+        path_to_csv_file = self._convert_meta_data_path_csv(
+            path_to_meta_data=path_to_meta_data
+        )
         data_frame = pandas.read_csv(
-            filepath_or_buffer=path_to_data_frame,
-            delimiter=",",
+            filepath_or_buffer=path_to_csv_file,
+            delimiter=meta_data_dictionary["delimiter"],
             parse_dates=date_columns,
             index_col=time_stamp_column_meta_data.index_column_name,
         )
 
-        software_run_data = SoftwareRunData(
-            **meta_data_dictionary["time_series_meta_data"]["software_run_data"]
-        )
+        source_data = self.get_source_data(meta_data_dict=meta_data_dictionary)
         list_of_column_meta_data = []
         for column_meta_data in meta_data_dictionary["time_series_meta_data"][
             "column_list"
@@ -63,7 +92,7 @@ class LoadProfileDataManager:
             name=meta_data_dictionary["time_series_meta_data"]["name"],
             data_frame=data_frame,
             column_list=list_of_column_meta_data,
-            software_run_data=software_run_data,
+            software_run_data=source_data,
             data_format_standard=meta_data_dictionary["data_format_standard"],
             first_start_time=meta_data_dictionary["time_series_meta_data"][
                 "first_start_time"
@@ -99,6 +128,102 @@ class LoadProfileDataManager:
             ]["end_column_number"],
         )
         return time_stamp_column_meta_data
+
+    def get_list_of_time_series_column_meta_data(
+        self, time_series_column_list: list[dict[str, str]]
+    ) -> list[TimeSeriesColumnEntryMetaData]:
+        list_of_time_series_meta_data = []
+        for time_series_column_dict in time_series_column_list:
+
+            if "description" in time_series_column_dict:
+                description = time_series_column_dict["description"]
+            else:
+                description = None
+
+            if "tags" in time_series_column_dict:
+                tags = time_series_column_dict["tags"]
+            else:
+                tags = None
+
+            if "min" in time_series_column_dict:
+
+                min = time_series_column_dict["min"]
+            else:
+                min = None
+            if "max" in time_series_column_dict:
+                max = time_series_column_dict["max"]
+            else:
+                max = None
+            list_of_time_series_meta_data.append(
+                TimeSeriesColumnEntryMetaData(
+                    column_number=time_series_column_dict["column_number"],
+                    column_name=time_series_column_dict["column_name"],
+                    unit=time_series_column_dict["unit"],
+                    description=description,
+                    tags=tags,
+                    min=min,
+                    max=max,
+                )
+            )
+        return list_of_time_series_meta_data
+
+    def get_source_data(
+        self, meta_data_dict: dict[str, dict[str, str]]
+    ) -> SoftwareSource | DataSource:
+        if DataSourceTypes.DATA_SOURCE in meta_data_dict:
+            source_meta_data = SoftwareSource(
+                source_type=DataSourceTypes.DATA_SOURCE,
+                source_name=meta_data_dict["source_name"],
+                reference=meta_data_dict["reference"],
+                guid=meta_data_dict["guid"],
+            )
+        elif DataSourceTypes.SOFTWARE_SOURCE in meta_data_dict:
+            source_meta_data = SoftwareSource(
+                source_type=DataSourceTypes.DATA_SOURCE,
+                software_name=meta_data_dict["software_name"],
+                version=meta_data_dict["version"],
+                user_name=meta_data_dict["user_name"],
+                guid=meta_data_dict["guid"],
+            )
+        else:
+            raise IncompleteData("Expected Either")
+        return source_meta_data
+
+    def create_data_frame(self):
+        pass
+
+    def write_meta_data(
+        self,
+        name: str,
+        data_frame: pandas.DataFrame,
+        source_meta_data: SoftwareSource | DataSource,
+        column_list: list[TimeSeriesColumnEntryMetaData],
+        start_column_name: str = "start",
+        end_column_name: str = "end",
+        index_column_name: str = "index",
+        delimiter=",",
+    ) -> TimeSeriesFileMetaData:
+        start_column_number = data_frame.columns.get_loc(start_column_name)
+        end_column_number = data_frame.columns.get_loc(end_column_name)
+        index_column_number = data_frame.columns.get_loc(index_column_name)
+        time_stamp_column_meta_data = TimeStampColumnMetaData(
+            index_column_number=index_column_number,
+            index_column_name=index_column_name,
+            start_column_name=start_column_name,
+            start_column_number=start_column_number,
+            end_column_name=end_column_name,
+            end_column_number=end_column_number,
+        )
+        times_series_standard = TimeSeriesFileMetaData(
+            name=name,
+            data_frame=data_frame,
+            time_stamp_column_meta_data=time_stamp_column_meta_data,
+            column_list=column_list,
+            source_meta_data=source_meta_data,
+            delimiter=delimiter,
+            times_series_standard=TimeSeriesStandards.V1_0,
+        )
+        return times_series_standard
 
 
 class JSADataManager:
